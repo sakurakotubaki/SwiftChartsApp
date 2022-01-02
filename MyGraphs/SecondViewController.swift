569fad4d018447833d112a04ab296b4c9ef5e515
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-01-02 20:42:31 +0900

    SecondViewControllerを作成

---
+++ MyGraphs/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  MyGraphs
+//
+//  Created by 橋本純一 on 2022/01/02.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
