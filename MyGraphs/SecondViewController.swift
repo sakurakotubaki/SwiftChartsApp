49182bd6c3b36fd3afc3826b9de4d8e6ebdd7c5b
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-01-02 21:17:17 +0900

    折れ線グラフ作れた

--- MyGraphs/SecondViewController.swift
+++ MyGraphs/SecondViewController.swift
@@ -4,15 +4,41 @@
 //
 //  Created by 橋本純一 on 2022/01/02.
 //
-
+// 折れ線グラフを作成
+import Charts
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, ChartViewDelegate {
+    
+    var lineChart = LineChartView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        lineChart.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
+        lineChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+        lineChart.center = view.center
+        
+        // 追加して保存
+        view.addSubview(lineChart)
+        // グラフとデータにエントリー
+        var entris = [ChartDataEntry]()
+        
+        for x in 0..<10 {
+            entris.append(ChartDataEntry(x: Double(x), // Double型にしないとエラー出る
+                                            y: Double(x))) // Double型にしないとエラー出る
+        }
+        
+        // 棒グラフのデータと等しい値を設定します
+        let set = LineChartDataSet(entries: entris)
+        // materialに変更
+        set.colors = ChartColorTemplates.material()
+        let data = LineChartData(dataSet: set)
+        lineChart.data = data
     }
 
 }
