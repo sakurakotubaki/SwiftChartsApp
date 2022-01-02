640b02099cd654867dabf8b1b843a68f1af50e0e
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-01-02 21:06:28 +0900

    棒グラフ作れた

--- MyGraphs/ViewController.swift
+++ MyGraphs/ViewController.swift
@@ -5,13 +5,45 @@
 //  Created by 橋本純一 on 2022/01/02.
 //
 
+import Charts
 import UIKit
 
-class ViewController: UIViewController {
+// ライブラリーを読み込む必要がある
+class ViewController: UIViewController, ChartViewDelegate {
+    
+    // 棒グラフを作成
+    var barChart = BarChartView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        barChart.delegate = self
+    }
+    
+    // 棒グラフのサブビューを作る
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        barChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+        barChart.center = view.center
+        
+        // 追加して保存
+        view.addSubview(barChart)
+        
+        var entris = [BarChartDataEntry]()
+        
+        for x in 0..<10 {
+            entris.append(BarChartDataEntry(x: Double(x), // Double型にしないとエラー出る
+                                            y: Double(x))) // Double型にしないとエラー出る
+        }
+        
+        // 棒グラフのデータと等しい値を設定します
+        let set = BarChartDataSet(entries: entris)
+        // カラフルな棒グラフが得られる
+        set.colors = ChartColorTemplates.joyful()
+        
+        let data = BarChartData(dataSet: set)
+        
+        barChart.data = data
     }
 
 
