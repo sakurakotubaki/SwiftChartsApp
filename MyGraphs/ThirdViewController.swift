0aaeb51b28dd20309cce7bdd56f20e9000a0bf8b
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-01-02 20:46:25 +0900

    3つのTabアプリを作った

--- MyGraphs/ThirdViewController.swift
+++ MyGraphs/ThirdViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  ThirdViewController.swift
 //  MyGraphs
 //
 //  Created by 橋本純一 on 2022/01/02.
@@ -7,12 +7,12 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class ThirdViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
-
+    
 }
